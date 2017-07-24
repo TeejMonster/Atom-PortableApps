572706a28275f4fddc0bc32ef41f322065e03080
TeejMonster <eliteh4xx0r@gmail.com>
2017-07-24 15:28:20 -0500

    Switch to Atom approved portablization
    Atom can run as a portable app without too much help.  This just adds a launcher in the right place so that Portable Apps can see it.

---
+++ Atom.bat
@@ -0,0 +1,5 @@
+REM |  The launcher in this directory is just this script turned into an EXE by
+REM |  Bat to EXE Converter by Fatih Kodak.
+REM |  http://www.f2ko.de/en/b2e.php
+
+start %~dp0Atom\atom.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
